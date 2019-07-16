881ec74606b44faa90d9e4b02a79a60a29b53e94
Luiz Eduardo C <eduselcip@gmail.com>
2019-07-16 00:33:28 -0300

    fix url env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: "http://191.252.59.5:1337",
+  apiUrl: "http://191.252.59.5:7100",
   adminId: "5d0aec202a3c39162cc786e2",
   viaCepUrl: "https://viacep.com.br/ws"
 };
